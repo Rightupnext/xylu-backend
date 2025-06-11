e8a860b29620462a53697b472e69295f278520dc
Sivabalan <sivabalanm205@gmail.com>
2025-06-11 11:01:05 +0530

    category completed

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -1,7 +1,11 @@
 exports.authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Access denied" });
+      return res
+        .status(403)
+        .json({
+          message: `Access denied. You do not have the required permissions for this action (your role: '${roles}').`,
+        });
     }
     next();
   };
---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const categoryController = require("../controllers/categoryController");
+const { authenticate } = require("../middleware/authMiddleware");
+const { authorizeRoles } = require("../middleware/roleMiddleware");
+const {encryptResponse,decryptMiddleware,} = require("../middleware/encryption");
+router.get("/",encryptResponse,decryptMiddleware, categoryController.getAllCategories);
+router.post("/", authenticate,authorizeRoles('admin','super-admin'),decryptMiddleware,encryptResponse,categoryController.createCategory);
+router.put("/:id",authenticate,authorizeRoles('admin','super-admin'),decryptMiddleware,encryptResponse, categoryController.updateCategoryById);
+router.delete("/:id",authenticate,authorizeRoles('admin','super-admin'),decryptMiddleware, encryptResponse,categoryController.deleteCategory);
+
+module.exports = router;
